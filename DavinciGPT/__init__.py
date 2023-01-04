8879838b860da01290f0dede2a720818ebbc12f0
SOME1HING <yashprakash2005@gmail.com>
2023-01-04 13:44:35 +0530

    hmm

--- DavinciGPT/__init__.py
+++ DavinciGPT/__init__.py
@@ -1,3 +1,7 @@
-from DavinciGPT.core.bot import Bot
+from DavinciGPT.config import *
+from pyrogram import Client
+from DavinciGPT.logger import LOGGER
+import sys
 
-app = Bot()
+
+app = Client("DavinciGPT", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
--- DavinciGPT/__main__.py
+++ DavinciGPT/__main__.py
@@ -4,6 +4,7 @@ from pyrogram import idle
 from DavinciGPT.plugins import ALL_MODULES
 from DavinciGPT import app
 from DavinciGPT.logger import LOGGER
+from DavinciGPT.config import LOG_GROUP_ID
 
 loop = asyncio.get_event_loop()
 
@@ -13,8 +14,14 @@ async def init():
     for all_module in ALL_MODULES:
         importlib.import_module("DavinciGPT.plugins" + all_module)
     LOGGER.info("Successfully Imported Modules ")
+    try:
+        await app.send_message(LOG_GROUP_ID, "DavinciGPT Started.")
+    except:
+        LOGGER.error("Bot wasn't able to send message in the log group.")
 
 
 if __name__ == "__main__":
+    LOGGER.info("DavinciGPT is starting.")
+    app.start()
     loop.run_until_complete(init())
-    LOGGER.info("Stopping DavinciGPT! GoodBye")
+    LOGGER.info("DavinciGPT started.")
