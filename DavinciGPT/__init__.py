76a6683e36d3fc7238d56b1e95692fba94dc5450
SOME1HING <yashprakash2005@gmail.com>
2023-01-04 13:46:56 +0530

    hmm

--- DavinciGPT/__init__.py
+++ DavinciGPT/__init__.py
@@ -1,7 +1,4 @@
 from DavinciGPT.config import *
 from pyrogram import Client
-from DavinciGPT.logger import LOGGER
-import sys
-
 
 app = Client("DavinciGPT", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
--- DavinciGPT/__main__.py
+++ DavinciGPT/__main__.py
@@ -1,6 +1,5 @@
 import asyncio
 import importlib
-from pyrogram import idle
 from DavinciGPT.plugins import ALL_MODULES
 from DavinciGPT import app
 from DavinciGPT.logger import LOGGER
@@ -10,7 +9,6 @@ loop = asyncio.get_event_loop()
 
 
 async def init():
-    await app.start()
     for all_module in ALL_MODULES:
         importlib.import_module("DavinciGPT.plugins" + all_module)
     LOGGER.info("Successfully Imported Modules ")
