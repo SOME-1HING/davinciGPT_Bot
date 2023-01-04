d347efbfa5846c935426b5d4e9cd5d2c2cc1e8b6
SOME1HING <yashprakash2005@gmail.com>
2023-01-04 13:57:25 +0530

    Update __main__.py

--- DavinciGPT/__main__.py
+++ DavinciGPT/__main__.py
@@ -1,5 +1,6 @@
 import asyncio
 import importlib
+from pyrogram import idle
 from DavinciGPT.plugins import ALL_MODULES
 from DavinciGPT import app
 from DavinciGPT.logger import LOGGER
@@ -11,17 +12,20 @@ loop = asyncio.get_event_loop()
 
 
 async def init():
+    LOGGER.info("DavinciGPT is starting.")
+    await app.start()
+    for all_module in ALL_MODULES:
+        importlib.import_module("DavinciGPT.plugins" + all_module)
+    LOGGER.info("Successfully Imported Modules ")
     try:
         await app.send_message(LOG_GROUP_ID, "DavinciGPT Started.")
     except:
         LOGGER.error("Bot wasn't able to send message in the log group.")
 
+    LOGGER.info("DavinciGPT started.")
+    await idle()
+
 
 if __name__ == "__main__":
-    LOGGER.info("DavinciGPT is starting.")
-    app.start()
-    for all_module in ALL_MODULES:
-        importlib.import_module("DavinciGPT.plugins" + all_module)
     loop.run_until_complete(init())
-    LOGGER.info("Successfully Imported Modules ")
-    LOGGER.info("DavinciGPT started.")
+    LOGGER.info("DavinciGPT Stopped.")
